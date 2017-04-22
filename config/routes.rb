ea16b4acf3f018de5bf34e155b2c06340e7760b7
Godfrey Chan <godfreykfc@gmail.com>
2017-04-22 15:12:08 -0700

    Change routes and bring back controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :statics
+  get "/:action", controller: "statics"
 
   root "statics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
