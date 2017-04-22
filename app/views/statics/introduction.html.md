d9a862f9c6a5001aca68d4fa377be4f565347424
Godfrey Chan <godfreykfc@gmail.com>
2017-04-22 15:45:15 -0700

    Filename parser

---
+++ app/views/statics/introduction.html.md
@@ -0,0 +1,25 @@
+Hello world
+
+
+
+```{foo/bar/filename.rs}rust
+ruby! {
+    class Calculator {
+        def plus(a: u64, b: u64) -> u64 {
+            a + b
+        }
+    }
+}
+```
+
+
+
+```rust
+ruby! {
+    class Calculator {
+        def plus(a: u64, b: u64) -> u64 {
+            a + b
+        }
+    }
+}
+```
--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,13 +1,52 @@
 require 'kramdown'
+require 'kramdown/parser'
+
+module Kramdown
+  module Parser
+    class Extras < GFM
+      FENCED_CODEBLOCK_PLUS_START = /^[ ]{0,3}[~`]{3,}\{?/
+      FENCED_CODEBLOCK_PLUS_MATCH = /^[ ]{0,3}(([~`]){3,})(?:\{(\S+)\})?\s*?((\S+?)(?:\?\S*)?)?\s*?\n(.*?)^[ ]{0,3}\1\2*\s*?\n/m
+
+      def initialize(*)
+        super
+
+        {:codeblock_fenced_gfm => :codeblock_fenced_gfm_plus}.each do |current, replacement|
+          i = @block_parsers.index(current)
+          @block_parsers.delete(current)
+          @block_parsers.insert(i, replacement)
+        end
+      end
+
+      define_parser(:codeblock_fenced_gfm_plus, FENCED_CODEBLOCK_PLUS_START, nil, 'parse_codeblock_fenced')
+
+      def parse_codeblock_fenced
+        if @src.check(self.class::FENCED_CODEBLOCK_PLUS_MATCH)
+          start_line_number = @src.current_line_number
+          @src.pos += @src.matched_size
+          el = new_block_el(:codeblock, @src[6], nil, :location => start_line_number)
+
+          if @src[3]
+            el.attr['data-filename'] = @src[3]
+          end
+
+          lang = @src[4].to_s.strip
+          unless lang.empty?
+            el.options[:lang] = lang
+            el.attr['class'] = "language-#{@src[5]}"
+          end
+          @tree.children << el
+          true
+        else
+          false
+        end
+      end
+    end
+  end
+end
 
 module MarkdownHandler
   KRAMDOWN_OPTIONS = {
-    input: 'GFM',              # Use GitHub-flavored markdown
-    coderay_css: :class,       # Output css classes to style
-    toc_levels: (2..3),        # Generate TOC from <h2>s and <h3>s only
-    syntax_highlighter_opts: {
-      line_number_anchors: false  # Don't add anchors to line numbers
-    }
+    input: 'Extras'
   }
 
   def self.erb
