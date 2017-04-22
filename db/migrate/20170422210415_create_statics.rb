ba93a4e79a84ae247ca8a52437aabcfd1a5cad27
Godfrey Chan <godfreykfc@gmail.com>
2017-04-22 15:03:54 -0700

    Some clean up

--- db/migrate/20170422210415_create_statics.rb
+++
@@ -1,8 +0,0 @@
-class CreateStatics < ActiveRecord::Migration[5.1]
-  def change
-    create_table :statics do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,4 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170422210415) do
-
-  create_table "statics", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
