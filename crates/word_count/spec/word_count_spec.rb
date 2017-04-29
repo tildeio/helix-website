e75639df1ff20650b7e5c4562cad1e3854422efb
Aaron Patterson <aaron.patterson@gmail.com>
2017-04-29 12:40:14 -0700

    Upper case string should be supported

--- crates/word_count/spec/word_count_spec.rb
+++ crates/word_count/spec/word_count_spec.rb
@@ -6,4 +6,11 @@ describe "WordCount" do
     expect(WordCount.search(path, "thee")).to eq(3034)
     expect(WordCount.ruby_search(path, "thee")).to eq(3034)
   end
+
+  it "can count uppercase strings" do
+    path = File.expand_path("fixtures/thee-by-quotes.csv", File.dirname(__FILE__))
+
+    expect(WordCount.search(path, "MALVOLIO")).to eq(1)
+    expect(WordCount.ruby_search(path, "MALVOLIO")).to eq(1)
+  end
 end
